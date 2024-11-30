807e32f9688e088261f33449bdc87cb79f1d1278
Jae-Hong-Kim <newfly101@naver.com>
2024-11-30 20:34:01 +0900

    [7] github-finder : querySelector 생성

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,9 @@
 export function on(target, eventName, handler) {
     target.addEventListener(eventName, handler);
 }
+
+export function qs(selector, scope = document) {
+    if (!selector) throw 'no Selector';
+
+    return scope.querySelector(selector);
+}
