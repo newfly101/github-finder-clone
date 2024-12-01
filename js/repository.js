0001702f47859ecdadc084c24b35d992a66b0a0a
Jae-Hong-Kim <newfly101@naver.com>
2024-12-02 02:19:47 +0900

    [14] github-finder : [repository] test 코드 삭제

--- js/repository.js
+++ js/repository.js
@@ -1,18 +1,5 @@
 const tag = '[Repository]';
 
-export async function fetchServerTest() {
-    try {
-        console.log(tag, "fetchServerTest");
-        const response = await fetch('http://localhost:3000/test', {
-            method: "GET",
-            credentials: 'include',
-        })
-        return await response.json();
-    } catch (e) {
-        console.log("error: ",e);
-    }
-}
-
 export async function fetchSearch(userName) {
     try {
         const response = await fetch(`http://localhost:3000/search-users?userName=${userName}`, {
