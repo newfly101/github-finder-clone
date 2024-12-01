e783522da3a8599811af9f99f4e108dcc9f3478a
Jae-Hong-Kim <newfly101@naver.com>
2024-12-01 13:08:32 +0900

    [9] github-finder : Store(저장소 초기화 추가)

--- js/Store.js
+++ js/Store.js
@@ -1,7 +1,16 @@
-
 const tag = '[STORE]';
 export default class Store {
     constructor() {
         console.log(tag);
+
+
+        this.searchUserName = "";
+        this.init();
     }
+
+    // 초기화
+    init() {
+        this.searchUserName = "";
+    }
+
 }
