f6fed4b7a2fbf175f0fe9ec089945547a075542d
Jae-Hong-Kim <newfly101@naver.com>
2024-12-01 13:09:47 +0900

    [9] github-finder : testing delete

--- js/Octokit.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { Octokit } = require("@octokit/rest");
-
-export const octokit = new Octokit({
-    auth: process.env.GH_TOKEN,  // 발급 받은 GitHub Personal Access Token
-});
-
