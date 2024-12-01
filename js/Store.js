7999690e6df9bb41077ff6ce79baecb373906697
Jae-Hong-Kim <newfly101@naver.com>
2024-12-02 01:01:50 +0900

    [12] github-finder : 비동기 처리 이후에 사용자 정보를 가져와서 화면에 뿌려줌 (repos, gists, follower, following)

--- js/Store.js
+++ js/Store.js
@@ -41,7 +41,28 @@ export default class Store {
             console.log("검색할 사용자의 이름을 입력해주세요.");
             this.init();
         }
-
     }
-
 }
+
+// this.findUser = {
+//     "login": "newfly101",
+//     "id": 62008619,
+//     "node_id": "MDQ6VXNlcjYyMDA4NjE5",
+//     "avatar_url": "https://avatars.githubusercontent.com/u/62008619?v=4",
+//     "gravatar_id": "",
+//     "url": "https://api.github.com/users/newfly101",
+//     "html_url": "https://github.com/newfly101",
+//     "followers_url": "https://api.github.com/users/newfly101/followers",
+//     "following_url": "https://api.github.com/users/newfly101/following{/other_user}",
+//     "gists_url": "https://api.github.com/users/newfly101/gists{/gist_id}",
+//     "starred_url": "https://api.github.com/users/newfly101/starred{/owner}{/repo}",
+//     "subscriptions_url": "https://api.github.com/users/newfly101/subscriptions",
+//     "organizations_url": "https://api.github.com/users/newfly101/orgs",
+//     "repos_url": "https://api.github.com/users/newfly101/repos",
+//     "events_url": "https://api.github.com/users/newfly101/events{/privacy}",
+//     "received_events_url": "https://api.github.com/users/newfly101/received_events",
+//     "type": "User",
+//     "user_view_type": "public",
+//     "site_admin": false,
+//     "score": 1
+// }
--- js/views/UserDetails.js
+++ js/views/UserDetails.js
@@ -8,20 +8,57 @@ export default class userDetails extends View {
         super(qs('.section2'));
         console.log("this", this);
 
-        this.userProfileImg = qs(".viewer-userProfile-img-container").firstElementChild;
+        this.userProfile = {
+            profileImg: "assets/userProfile.svg",
+            htmlUrl: "#",
+            profileOptions: {
+                repository: "",
+                gists: "",
+                Followers: "",
+                Followings: ""
+            },
+            // api 다시 재 요청 해야함
+            // userFileOptions: {
+            //     company: "",
+            //     website: "",
+            //     location: "",
+            //     since: "",
+            // }
+        }
+
+        this.userProfile.profileImg = qs(".viewer-userProfile-img-container").firstElementChild;
+        this.userProfile.htmlUrl = qs("#view-to-github");
+        this.userProfile.profileOptions.repository = qs("#user-1");
+        this.userProfile.profileOptions.gists = qs("#user-2");
+        this.userProfile.profileOptions.Followers = qs("#user-3");
+        this.userProfile.profileOptions.Followings = qs("#user-4");
+        // this.userProfile.userFileOptions.company = qs("#userFile-1");
+        // this.userProfile.userFileOptions.website = qs("#userFile-2");
+        // this.userProfile.userFileOptions.location = qs("#userFile-3");
+        // this.userProfile.userFileOptions.since = qs("#userFile-4");
+        // this.userProfile.
 
         this.bindEvent();
     }
 
     async getUserProfile(userProfile) {
-        this.userProfileImg.src = userProfile.avatar_url;
-        console.log(this.userProfileImg);
+        this.userProfile.profileImg.src = userProfile.avatar_url;
+        this.userProfile.htmlUrl.href = userProfile.html_url;
+        this.userProfile.profileOptions.repository.innerText = userProfile.repos_url.length;
+        this.userProfile.profileOptions.gists.innerText = userProfile.gists_url.length;
+        this.userProfile.profileOptions.Followers.innerText = userProfile.followers_url.length;
+        this.userProfile.profileOptions.Followings.innerText = userProfile.following_url.length;
+        // console.log(this.userProfile.profileOptions);
+        // this.userProfile.userFileOptions.company.innerText = userProfile.url.company;
+        // this.userProfile.userFileOptions.website.innerText = userProfile.url.blog;
+        // this.userProfile.userFileOptions.location.innerText = userProfile.url.location;
+        // this.userProfile.userFileOptions.since.innerText = userProfile.url.created_at;
     }
 
     bindEvent() {
         on(this.element, "getUser", () => this.handleGetUser());
     }
     handleGetUser() {
-        this.emit("@getUser", this.userProfileImg);
+        this.emit("@getUser", this.userProfile.profileImg);
     }
 }
