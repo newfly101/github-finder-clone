be1121101a9d83891d07d22d8498cd890eb70069
Jae-Hong-Kim <newfly101@naver.com>
2024-12-02 11:23:03 +0900

    [15] github-finder : 각 repos, follower, following, gists 의 url로 요청을 날려, object.length를 계산하여 넣음

--- js/Store.js
+++ js/Store.js
@@ -1,4 +1,4 @@
-import {fetchSearch} from "./repository.js";
+import {fetchSearch, fetchToUrl} from "./repository.js";
 
 const tag = '[STORE]';
 export default class Store {
@@ -14,19 +14,53 @@ export default class Store {
     init() {
         this.searchUserName = "";
     }
+    // 정규식을 이용해서 object의 value에 {/secret} 과 같이 {} 문자열이 있으면 삭제
+    clearOptionStrings(object) {
+        return Object.fromEntries(
+            Object.entries(object).map(([key, value]) => {
+                if (typeof value === "string") {
+                    value = value.replace(/\{[^}]*\}/g, "");
+                }
+                return [key, value];
+            })
+        );
+    }
 
     async getSearchUser(userName) {
         if (userName.trim().length > 0) {
             try {
                 const data = await fetchSearch(userName);
                 this.findUser = data.find(user => user.login === userName);
+                this.findUser = this.clearOptionStrings(this.findUser);
                 console.log(tag,"SearchUser => ",this.findUser);
+                await this.calcDetailCount();
             } catch (error) {
                 console.log(tag, 'Error in SearchUser:', error);
             }
         } else {
             console.log("검색할 사용자의 이름을 입력해주세요.");
             this.init();
+        }
+    }
+
+    async calcDetailCount() {
+        console.log(tag, "Calculating each button's Count");
+        try{
+            const follower = await fetchToUrl(this.findUser.followers_url);
+            this.findUser.followersCount = follower.length;
+
+            const following = await fetchToUrl(this.findUser.following_url);
+            this.findUser.followingCount = following.length;
+
+            const repos = await fetchToUrl(this.findUser.repos_url);
+            this.findUser.reposCount = repos.length;
+
+            const gists = await fetchToUrl(this.findUser.gists_url);
+            this.findUser.gistsCount = gists.length;
+
+            console.log(tag, "Calculating Complete Count");
+        } catch (e) {
+            console.log(tag, 'Error in SearchUserFollow:', e);
         }
     }
 
--- js/repository.js
+++ js/repository.js
@@ -7,9 +7,23 @@ export async function fetchSearch(userName) {
             headers: {
                 'Content-Type': 'application/json',
             }
-        })
+        });
         return await response.json();
     } catch (error) {
         console.error('Error fetching data:', error);
+    }
+}
+
+export async function fetchToUrl(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching api.github.com :", error);
     }
 }
